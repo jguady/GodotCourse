8a0f9ca2974b3e2d53a0768d81a4939b15fd59c2
Jay Guadagnoli <jguadycsmail@gmail.com>
2024-05-05 22:30:18 -0600

    Pause, Options, Main Menu Screens

--- 2dSurvivorsCourse/scenes/main/main.gd
+++ 2dSurvivorsCourse/scenes/main/main.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @export var end_screen_scene: PackedScene
+var pause_menu_scene = preload("res://scenes/ui/pause_menu.tscn")
 
 func _ready():
 	%Player.health_component.died.connect(on_player_died)
@@ -11,3 +12,7 @@ func on_player_died():
 	add_child(end_screen_instance)
 	end_screen_instance.set_defeat()
 	
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		add_child(pause_menu_scene.instantiate())
+		get_tree().root.set_input_as_handled()
