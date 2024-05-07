2f863cbc41bf7262cbdab382c2bd74b244a0c025
Jay Guadagnoli <jguadycsmail@gmail.com>
2024-05-06 20:29:12 -0600

    Screen Transition, meta progression, and more events

--- 2dSurvivorsCourse/scenes/ui/pause_menu.gd
+++ 2dSurvivorsCourse/scenes/ui/pause_menu.gd
@@ -42,6 +42,8 @@ func close():
 	queue_free()
 
 func on_options_pressed():
+	ScreenTransition.transition()
+	await ScreenTransition.transitioned_halfway
 	var options_menu_instance = options_menu_scene.instantiate()
 	add_child(options_menu_instance)
 	options_menu_instance.back_pressed.connect(on_options_back_pressed.bind(options_menu_instance))
@@ -49,6 +51,8 @@ func on_options_pressed():
 	
 	
 func on_quit_pressed():
+	ScreenTransition.transition()
+	await ScreenTransition.transitioned_halfway
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
 
