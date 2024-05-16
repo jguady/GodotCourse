8500b58ee3645c1b640ab4465ba1d89300127d44
Jay Guadagnoli <jguadycsmail@gmail.com>
2024-05-16 17:09:44 -0600

    Added MetaProgression, Experience_Gain Meta Card, and Meta menu

--- 2dSurvivorsCourse/resources/meta_upgrades/meta_upgrade.gd
+++ 2dSurvivorsCourse/resources/meta_upgrades/meta_upgrade.gd
@@ -4,7 +4,9 @@ class_name MetaUpgrade
 @export var id: String
 @export var max_quantity: int = 1
 @export var title: String
+@export var experience_cost: int = 10
 @export_multiline var description: String
+
 
 
 
