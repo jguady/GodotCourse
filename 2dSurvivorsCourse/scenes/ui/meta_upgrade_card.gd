9dc80b71757bc8bc666ccd03437467ef3dc84de1
Jay Guadagnoli <jguadycsmail@gmail.com>
2024-05-30 22:26:08 -0600

    meta_upgrade

--- 2dSurvivorsCourse/scenes/ui/meta_upgrade_card.gd
+++ 2dSurvivorsCourse/scenes/ui/meta_upgrade_card.gd
@@ -5,6 +5,7 @@ extends PanelContainer
 @onready var progress_bar: ProgressBar = %ProgressBar
 @onready var purchase_button: Button = %PurchaseButton
 @onready var progress_label: Label = %ProgressLabel
+@onready var count_label: Label = %"Count Label"
 
 var upgrade: MetaUpgrade
 
@@ -18,13 +19,18 @@ func set_meta_upgrade(upgrade: MetaUpgrade):
 	update_progress()
 
 func update_progress():
+	var current_quantity = MetaProgression.save_data["meta_upgrades"][upgrade.id]["quantity"]
+	var is_maxed = current_quantity >= upgrade.max_quantity
 	var currency = MetaProgression.save_data["meta_upgrade_currency"]
 	var percent =  currency / upgrade.experience_cost
 	percent = min(percent, 1)
 	progress_bar.value = percent
-	purchase_button.disabled = percent < 1
+	purchase_button.disabled = percent < 1 || is_maxed
+	if is_maxed: 
+		purchase_button.text = "Maximum Acquired"
 	purchase_button.disabled = false
 	progress_label.text = str(currency)+ "/" + str(upgrade.experience_cost)
+	count_label.text = "x%d" % current_quantity
 	
 	
 func select_card():
